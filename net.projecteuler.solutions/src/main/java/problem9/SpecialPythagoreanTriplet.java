f78b877efe8fbc022d45ab0a66fc7a4c74eb15ec
Miguel <miguel.trigueira@gmail.com>
2024-09-30 15:59:04 +0100

    Problem 9 Special Pythagorean Triplet

---
+++ net.projecteuler.solutions/src/main/java/problem9/SpecialPythagoreanTriplet.java
@@ -0,0 +1,32 @@
+package problem9;
+
+public class SpecialPythagoreanTriplet {
+    public static void main(String[] args) {
+        System.out.println(productWhenSumEquals(1000));
+    }
+
+    public static int productWhenSumEquals(int sum) {
+        int limit = sum - (1 + 2);
+
+        for (int a = 1; a < limit - 2; a++)
+            for (int b = a + 1; b < limit - 1; b++) {
+                int c = calculateC(a, b, sum);
+                if (isPythagoreanTriplet(a, b, c))
+                    return product(a, b, c);
+            }
+
+        return -1;
+    }
+
+    private static int calculateC(int a, int b, int sum) {
+        return sum - a - b;
+    }
+
+    private static boolean isPythagoreanTriplet(int a, int b, int c) {
+        return a * a + b * b == c * c;
+    }
+
+    private static int product(int a, int b, int c) {
+        return a * b * c;
+    }
+}
---
+++ net.projecteuler.solutions/src/test/java/problem9/Triplet.java
@@ -0,0 +1,23 @@
+package problem9;
+
+class Triplet {
+    public static Triplet of(int a, int b, int c) {
+        return new Triplet(a, b, c);
+    }
+
+    private final int a, b, c;
+
+    private Triplet(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    final int sum() {
+        return a + b + c;
+    }
+
+    final int product() {
+        return a * b * c;
+    }
+}
