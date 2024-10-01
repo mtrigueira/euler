2d88ed1c3baf9c665a13ece36c7a14193bdec1d4
Miguel <miguel.trigueira@gmail.com>
2024-10-01 18:10:01 +0100

    Problem 13 Large Sum

---
+++ net.projecteuler.solutions/src/test/java/problem13/LargeSumTest.java
@@ -0,0 +1,26 @@
+package problem13;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LargeSumTest {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "0:0",
+            "1:0,1",
+            "12:0,1,11",
+            "12345678901234567890123456789012345678901234567890:12345678901234567890123456789012345678901234567890",
+            "12345678901234567890123456789012345678901234567891:12345678901234567890123456789012345678901234567890,1",
+            "999999999999999999999999999999999999999999999999999:12345678901234567890123456789012345678901234567890,987654321098765432109876543210987654321098765432109",
+            "1000000000000000000000000000000000000000000000000000:12345678901234567890123456789012345678901234567890,987654321098765432109876543210987654321098765432109,1"
+    })
+    void sum(String testRow) {
+        String[] temp = testRow.split(":");
+        String expected = temp[0];
+        String[] numbers = temp[1].split(",");
+
+        assertEquals(expected, LargeSum.sum(numbers));
+    }
+}
