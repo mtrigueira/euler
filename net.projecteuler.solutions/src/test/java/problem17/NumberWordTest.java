ad9df41aab0ebc7490113ea112200e45c9987691
Miguel <miguel.trigueira@gmail.com>
2024-10-02 14:51:49 +0100

    Problem 17 Number Letter Counts

---
+++ net.projecteuler.solutions/src/test/java/problem17/NumberWordTest.java
@@ -0,0 +1,34 @@
+package problem17;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumberWordTest {
+    static int indexIsNumber = 1;
+    static int indexIsTen = 1;
+    static int indexIsOneHundredAnd = 0;
+
+    @ParameterizedTest
+    @ValueSource(strings =
+            {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+                    "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five",
+                    "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty"})
+    void of1digit(String word) {
+        assertEquals(word, NumberWord.of(indexIsNumber++));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings =
+            {"ten", "twenty", "thirty","forty","fifty","sixty","seventy", "eighty", "ninety", "one hundred"})
+    void ofTens(String word) {
+        assertEquals(word, NumberWord.of(10*indexIsTen++));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"one hundred", "one hundred and one", "one hundred and two"})
+    void ofHundredAnds(String word) {
+        assertEquals(word, NumberWord.of(100+indexIsOneHundredAnd++));
+    }
+}
