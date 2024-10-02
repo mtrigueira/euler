03c7a0d07adefca7f864aa6f377236bf874ed87b
Miguel <miguel.trigueira@gmail.com>
2024-10-02 12:57:42 +0100

    Problem 15 Lattice Paths

---
+++ net.projecteuler.solutions/src/main/java/problem15/LatticePaths.java
@@ -0,0 +1,42 @@
+package problem15;
+
+public class LatticePaths {
+    public static void main(String[] args) {
+        // https://projecteuler.net/problem=15
+        System.out.println(LatticePaths.count(20));
+    }
+
+    public static long count(int i) {
+        return new LatticePaths(i).count();
+    }
+
+    private final int n;
+    private final long[][] lattice;
+    private LatticePaths(int n) {
+        this.n = n;
+        lattice = createStartingLattice(n);
+    }
+
+    private long count() {
+        if (n < 1) return 0;
+
+        // The lattice is symmetrical along the dexter diagonal (NW-SE),
+        // This might be useful for future optimization if needed
+        for (int j = 1; j <= n; j++)
+            for (int i = j; i <= n; i++) {
+                lattice[j][i] = lattice[j - 1][i] + lattice[j][i - 1];
+                lattice[i][j] = lattice[i][j - 1] + lattice[i - 1][j];
+            }
+
+        return lattice[n][n];
+    }
+
+    private long[][] createStartingLattice(int n) {
+        long[][] lattice = new long[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++)
+            lattice[0][i] = lattice[i][0] = 1;
+
+        return lattice;
+    }
+}
---
+++ net.projecteuler.solutions/src/test/java/problem15/LatticePathsTest.java
@@ -0,0 +1,33 @@
+package problem15;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LatticePathsTest {
+
+    @Test
+    void test0() {
+        assertEquals(0, LatticePaths.count(0));
+    }
+
+    @Test
+    void test1() {
+        assertEquals(2, LatticePaths.count(1));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(6, LatticePaths.count(2));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(20, LatticePaths.count(3));
+    }
+
+    @Test
+    void test4() {
+        assertEquals(70, LatticePaths.count(4));
+    }
+}
