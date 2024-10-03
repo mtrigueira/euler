2d561317c9f6e7b1825adc1dfb92bf2ac9b07d4e
Miguel <miguel.trigueira@gmail.com>
2024-10-03 09:32:10 +0100

    Problem 19 Counting Sundays

---
+++ net.projecteuler.solutions/src/main/java/problem19/CountingSundays.java
@@ -0,0 +1,41 @@
+package problem19;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
+public class CountingSundays {
+    public static void main(String[] args) {
+        // https://projecteuler.net/problem=19
+        System.out.println(between(1901, 2000));
+    }
+
+    static int between(int startYear, int endYear) {
+        return between(Year.of(startYear), Year.of(endYear));
+    }
+
+    static int between(Year startYear, Year endYear) {
+        int sundays = 0;
+
+        for (Year year = startYear; year.getValue()<=endYear.getValue(); year = year.plusYears(1))
+            sundays += numberOfSundaysInAYear(year);
+
+        return sundays;
+    }
+
+    static int numberOfSundaysInAYear(Year year) {
+        int sundays = 0;
+
+        for (Month month : Month.values())
+            if (firstDayOfWeekIsASunday(year, month))
+                sundays += 1;
+
+        return sundays;
+    }
+
+    static boolean firstDayOfWeekIsASunday(Year year, Month month) {
+        LocalDate date = year.atMonth(month).atDay(1);
+        return (DayOfWeek.from(date).equals(DayOfWeek.SUNDAY));
+    }
+}
