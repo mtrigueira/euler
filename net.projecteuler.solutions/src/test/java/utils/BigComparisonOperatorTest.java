4bd05277bf9082f9db6c5b18fa32d18ff507e9b7
Miguel <miguel.trigueira@gmail.com>
2024-10-05 08:41:11 +0100

    BigInteger Comparison Operators

---
+++ net.projecteuler.solutions/src/test/java/utils/BigComparisonOperatorTest.java
@@ -0,0 +1,68 @@
+package utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static utils.BigComparisonOperator.*;
+
+class BigComparisonOperatorTest {
+    private static final BigInteger LONG_MAX_VALUE = BigInteger.valueOf(Long.MAX_VALUE);
+    private static final BigInteger LONG_MIN_VALUE = BigInteger.valueOf(Long.MIN_VALUE);
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1, 2, 3, 4, 5, 6, 7})
+    void test(int i) {
+        BigInteger value = BigInteger.valueOf(i);
+
+        assertValueComparedWithItself(value);
+        assertValueComparedWithABiggerValue(value);
+        assertValueComparedWithASmallerValue(value);
+    }
+
+    private static void assertValueComparedWithItself(BigInteger value) {
+        // a == b
+        BigInteger a = value;
+        assertFalse(lessThan(a, a));
+        assertTrue(lessThanOrEqual(a, a));
+        assertTrue(equal(a, a));
+        assertFalse(greaterThan(a, a));
+        assertTrue(greaterThanOrEqual(a, a));
+        assertShortcuts(a, a);
+    }
+
+
+    private static void assertValueComparedWithABiggerValue(BigInteger value) {
+        // a < b
+        BigInteger a = value;
+        BigInteger b = LONG_MAX_VALUE;
+        assertTrue(lessThan(a, b));
+        assertTrue(lessThanOrEqual(a, b));
+        assertFalse(equal(a, b));
+        assertFalse(greaterThan(a, b));
+        assertFalse(greaterThanOrEqual(a, b));
+        assertShortcuts(a, b);
+    }
+
+    private static void assertValueComparedWithASmallerValue(BigInteger value) {
+        // a > b
+        BigInteger a = value;
+        BigInteger b = LONG_MIN_VALUE;
+        assertFalse(lessThan(a, b));
+        assertFalse(lessThanOrEqual(a, b));
+        assertFalse(equal(a, b));
+        assertTrue(greaterThan(a, b));
+        assertTrue(greaterThanOrEqual(a, b));
+        assertShortcuts(a, b);
+    }
+
+    private static void assertShortcuts(BigInteger a, BigInteger b) {
+        assertEquals(lessThan(a, b), lt(a, b));
+        assertEquals(lessThanOrEqual(a, b), lte(a, b));
+        assertEquals(equal(a, b), eq(a, b));
+        assertEquals(greaterThan(a, b), gt(a, b));
+        assertEquals(greaterThanOrEqual(a, b), gte(a, b));
+    }
+}
