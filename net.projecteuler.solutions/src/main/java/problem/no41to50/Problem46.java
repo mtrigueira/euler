deba9838b92242fe428b1aeb0efad3ecbd811ba0
Miguel <miguel.trigueira@gmail.com>
2024-10-14 16:11:25 +0100

    Problem 46 Goldbach's other conjecture

---
+++ net.projecteuler.solutions/src/main/java/problem/no41to50/Problem46.java
@@ -0,0 +1,39 @@
+package problem.no41to50;
+
+import utils.sequence.arithmetic.CompositeSequence;
+import utils.sequence.arithmetic.PrimeSequence;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+import static java.math.BigInteger.TWO;
+import static problem.Solution.problem;
+import static problem.Solution.solution;
+
+public class Problem46 {
+    public static void main(String[] args) {
+        problem("Goldbach's other conjecture");
+        solution(goldbachsOtherConjecture().map(BigInteger::toString).orElse("Not found"));
+    }
+
+    private static Optional<BigInteger> goldbachsOtherConjecture() {
+        return new CompositeSequence().stream()
+                .filter(x->x.testBit(0))
+                .filter(x->!canBeWrittenAsSumOfPrimesAnd2TimesSquare(x))
+                .limit(1).findAny();
+
+    }
+
+    private static boolean canBeWrittenAsSumOfPrimesAnd2TimesSquare(BigInteger x) {
+        return PrimeSequence.fromFirst().stream()
+                .takeWhile(p->p.compareTo(x)<0)
+                .anyMatch(p->
+                        x.subtract(p)
+                                .divide(TWO)
+                                .sqrt()
+                                .pow(2)
+                                .multiply(TWO)
+                                .add(p)
+                                .equals(x));
+    }
+}
---
+++ net.projecteuler.solutions/src/main/java/utils/sequence/arithmetic/CompositeSequence.java
@@ -0,0 +1,25 @@
+package utils.sequence.arithmetic;
+
+import utils.prime.Prime;
+
+import java.math.BigInteger;
+
+import static java.math.BigInteger.ONE;
+
+public class CompositeSequence extends ArithmeticNumberSequence<BigInteger> {
+    private final PrimeSequence p = PrimeSequence.from(5);
+    private Prime lastPrime = p.next();
+    private BigInteger i = BigInteger.valueOf(4);
+
+    @Override
+    public BigInteger next() {
+        BigInteger current = i;
+        i = i.add(ONE);
+        if(lastPrime.equals(i)) {
+            lastPrime = p.next();
+            i = i.add(ONE);
+        }
+
+        return current;
+    }
+}
