6c06bca1c821a870cedd93aed60b9f91026d0672
Miguel <miguel.trigueira@gmail.com>
2024-10-23 16:53:44 +0100

    Problem 61 revised

--- net.projecteuler.solutions/src/main/java/problem/no61to70/Problem61.java
+++ net.projecteuler.solutions/src/main/java/problem/no61to70/Problem61.java
@@ -3,9 +3,7 @@ package problem.no61to70;
 import utils.polyagonal.Polygonal;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 import static problem.Solution.problem;
 import static problem.Solution.solution;
@@ -15,106 +13,128 @@ public class Problem61 {
     public static void main(String[] args) {
         // https://projecteuler.net/problem=61
         problem("Cyclical figurate numbers");
-        solution(cyclicNumbersWithUniqueNagonals(6));
+        solution(cyclicNumbersWithUniquePolygonals(6));
     }
 
-    static int cyclicNumbersWithUniqueNagonals(int count) {
-     List<Polygonal> POLYGONAL_LIST = POLYGONALS.subList(0, count);
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1000; i <= 9999; i++)
-            numbers.add(i);
+    static int cyclicNumbersWithUniquePolygonals(int count) {
+        List<Polygonal> POLYGONAL_LIST = POLYGONALS.subList(0, count);
+        List<Node> numbers = createNodes(POLYGONAL_LIST);
 
-        List<List<Integer>> pairs = new ArrayList<>();
-        for (int i = 0; i < numbers.size(); i++) {
-            Integer m = numbers.get(i);
-            for (int j = 0; j < numbers.size(); j++) {
-                if(i==j) continue;
-                Integer n = numbers.get(j);
-                if (areCyclic(m, n) && areDifferentPolygonals(POLYGONAL_LIST, m, n)) {
-                    pairs.add(new ArrayList<>(List.of(m, n)));
+        addNextInChainToNodes(numbers);
+        numbers = numbers.stream().filter(n -> !n.nextInChain.isEmpty()).toList();
+
+        List<List<Node>> cycles = findCycles(numbers);
+
+        cycles=cycles.stream().filter(c ->c.size()==count).toList();
+
+        cycles.stream().map(c ->
+                c.stream().map(n -> n.i + " "+n.polygonal.toString()).toList()+" "+
+                        c.stream().mapToInt(n -> n.i).sum()
+        ).forEach(System.out::println);
+
+        return cycles.get(0).stream().mapToInt(n -> n.i).sum();
+    }
+
+    private static List<List<Node>> findCycles(List<Node> nodes) {
+        List<List<Node>> cycles = new ArrayList<>();
+
+        for (Node node : nodes) {
+            List<List<Node>> chains = chain(node);
+            for(List<Node> chain : chains)
+                if (isCyclical(chain)) {
+                    sortChain(chain);
+                    if (!cycles.contains(chain))
+                        cycles.add(chain);
+                }
+        }
+
+        return cycles;
+    }
+
+    private static boolean isCyclical(List<Node> chain) {
+        return chain.get(chain.size() - 1).nextInChain.contains(chain.get(0));
+    }
+
+    private static void sortChain(List<Node> chain) {
+        Node min = chain.stream().min(Comparator.comparingInt(a -> a.i)).orElseThrow();
+        int minIndex = chain.indexOf(min);
+
+        Collections.rotate(chain, -minIndex);
+    }
+
+    private static List<List<Node>> chain(Node node) {
+        return chain(node, new ArrayList<>());
+    }
+
+    private static List<List<Node>> chain(Node node, List<Node> chain) {
+        if (node.nextInChain.isEmpty()) return List.of();
+        if (chain.contains(node)) {
+            return List.of(chain);
+        }
+        List<List<Node>> childChains = new ArrayList<>();
+        List<Node> newChain = new ArrayList<>(chain);
+        newChain.add(node);
+        if (areDifferentPolygonals(newChain.toArray(Node[]::new))) {
+            for (int i = 0; i < node.nextInChain.size(); i++) {
+                List<List<Node>> childChain = chain(node.nextInChain.get(i), newChain);
+                if (!childChain.isEmpty()) {
+                    childChains.addAll(childChain);
                 }
             }
+            if (childChains.isEmpty())
+                return List.of(newChain);
         }
-        pairs = pairs.stream().distinct().toList();
 
-        List<List<Integer>> bigCycle = upcycle(pairs, pairs, POLYGONAL_LIST);
-        if(count>=4)
-            bigCycle = upcycle(bigCycle, pairs, POLYGONAL_LIST);
-        if(count>=5)
-            bigCycle = upcycle(bigCycle, pairs, POLYGONAL_LIST);
-        if(count>=6)
-            bigCycle = upcycle(bigCycle, pairs, POLYGONAL_LIST);
-        bigCycle = bigCycle.stream().filter(a -> areCyclic(a.get(count-1), a.get(0))).toList();
-
-        bigCycle = bigCycle.stream().filter(a -> a.stream().sorted().findFirst().orElse(-1).equals(a.get(0))).toList();
-
-        bigCycle.stream()
-                .peek(a -> dumpDifferentPolygonals(new ArrayList<>(POLYGONAL_LIST), a.toArray(Integer[]::new)))
-                .map(List::toString)
-                .forEach(System.out::println);
-
-        return bigCycle.stream().findFirst().stream().flatMap(Collection::stream).mapToInt(Integer::intValue).sum();
+        return childChains;
     }
 
-    private static boolean areDifferentPolygonals(List<Polygonal> polygonals, Integer... m) {
-        List<Polygonal> isMPolygonal = polygonals.stream().filter(p -> p.is(BigInteger.valueOf(m[0]))).toList();
-        if (polygonals.stream().anyMatch(p -> p.is(BigInteger.valueOf(m[0])))) {
-            if (m.length == 1) {
-                return true;
+    private static void addNextInChainToNodes(List<Node> numbers) {
+        for (Node m : numbers)
+            for (Node n : numbers) {
+                int i = m.i;
+                int j = n.i;
+                if (i == j) continue;
+
+                if (areChained(i, j))
+                    if(areDifferentPolygonals(m,n))
+                        m.nextInChain.add(n);
             }
-            Integer[] newM = new Integer[m.length - 1];
-            System.arraycopy(m, 1, newM, 0, m.length - 1);
-            for (Polygonal p : isMPolygonal) {
-                if (areDifferentPolygonals(polygonals.stream().filter(obj -> !p.equals(obj)).toList(), newM)) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
-
-    private static boolean dumpDifferentPolygonals(List<Polygonal> polygonals, Integer... m) {
-        List<Polygonal> isMPolygonal = polygonals.stream().filter(p -> p.is(BigInteger.valueOf(m[0]))).toList();
-        if (polygonals.stream().anyMatch(p -> p.is(BigInteger.valueOf(m[0])))) {
-            if (m.length == 1) {
-                System.out.println(m[0] + " is " + polygonals);
-                return true;
-            }
-            Integer[] newM = new Integer[m.length - 1];
-            System.arraycopy(m, 1, newM, 0, m.length - 1);
-            for (Polygonal p : isMPolygonal) {
-                if (dumpDifferentPolygonals(polygonals.stream().filter(obj -> !p.equals(obj)).toList(), newM)) {
-                    System.out.println(m[0] + " is " + p);
-                    return true;
-                }
-            }
+    private static List<Node> createNodes(List<Polygonal> POLYGONAL_LIST) {
+        List<Node> numbers = new ArrayList<>();
+        for (int i = 1000; i <= 9999; i++) {
+            List<Polygonal> list = polygonalsOf(POLYGONAL_LIST, i);
+            if (!list.isEmpty())
+                numbers.add(new Node(i, list, new ArrayList<>()));
         }
-        return false;
+        return numbers;
     }
 
-    private static boolean areCyclic(Integer m, Integer n) {
+    private static List<Polygonal> polygonalsOf(List<Polygonal> POLYGONAL_LIST, int i) {
+        var b = BigInteger.valueOf(i);
+        return POLYGONAL_LIST.stream().filter(p -> p.is(b)).toList();
+    }
+
+    static boolean areDifferentPolygonals(Node... m) {
+        List<List<Polygonal>> polygonals = Arrays.stream(m).toList().stream().map(n -> n.polygonal).toList();
+        List<List<Polygonal>> distinctPolygonals = polygonals.stream().distinct().toList();
+        if(distinctPolygonals.size()<m.length) return false;
+
+        for (List<Polygonal> polygonal : distinctPolygonals)
+            if (Collections.frequency(polygonals, polygonal) > 1) return false;
+
+        return true;
+    }
+
+    private static boolean areChained(Integer m, Integer n) {
         return m % 100 == n / 100;
     }
 
-    static List<List<Integer>> upcycle(List<List<Integer>> cycles, List<List<Integer>> pairs, List<Polygonal> polygonals) {
-        List<List<Integer>> out = new ArrayList<>();
-        for (List<Integer> cycle : cycles) {
-            List<Integer> integers = new ArrayList<>(cycle);
-            Integer o = integers.get(integers.size() - 1);
-            for (List<Integer> pair : pairs) {
-                Integer o2 = pair.get(0);
-                if (o.equals(o2)) {
-                    List<Integer> m = new ArrayList<>(integers);
-                    Integer p = pair.get(1);
-                    m.add(p);
-                    if (areDifferentPolygonals(polygonals, m.toArray(Integer[]::new))) {
-                        out.add(m);
-                    }
-                }
-            }
+    public record Node(int i, List<Polygonal> polygonal, List<Node> nextInChain) {
+        @Override
+        public String toString() {
+            return i + " is " + polygonal + ", next " + nextInChain.stream().map(n -> n.i).toList();
         }
-        out = out.stream().distinct().toList();
-        return out;
     }
 }
