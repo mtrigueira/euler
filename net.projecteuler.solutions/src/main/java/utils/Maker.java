6e5faef6de96c3c22c79704df0478e0664bb9ce1
Miguel <miguel.trigueira@gmail.com>
2024-10-24 16:36:27 +0100

    Problem 64 Odd Period Square Roots

--- net.projecteuler.solutions/src/main/java/utils/Maker.java
+++ net.projecteuler.solutions/src/main/java/utils/Maker.java
@@ -3,10 +3,15 @@ package utils;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class SetMaker {
+public class Maker {
     public static Set<BigInteger> set(int... values) {
         return Arrays.stream(values).asLongStream().mapToObj(BigInteger::valueOf).collect(HashSet::new, HashSet::add, HashSet::addAll);
+    }
+
+    public static List<BigInteger> list(int... values) {
+        return Arrays.stream(values).asLongStream().mapToObj(BigInteger::valueOf).toList();
     }
 }
