ff65283ad08fd5170bb44ec708f775dd4d59a892
Miguel <miguel.trigueira@gmail.com>
2024-11-12 09:52:32 +0000

    Problem 50 BLUE

--- net.projecteuler.solutions/src/test/java/problem/no41to50/Problem50Test.java
+++ net.projecteuler.solutions/src/test/java/problem/no41to50/Problem50Test.java
@@ -7,13 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Problem50Test {
     @Test
     void test100() {
-        assertEquals(41, Problem50.largestConsecutivePrimeSumBelow(100).orElseThrow().intValueExact());
+        assertEquals(41, Problem50.largestConsecutivePrimeSumBelow(100).orElseThrow());
     }
 
     @Test
     void test1000() {
-        assertEquals(953, Problem50.largestConsecutivePrimeSumBelow(1000).orElseThrow().intValueExact());
+        assertEquals(953, Problem50.largestConsecutivePrimeSumBelow(1000).orElseThrow());
     }
-
-
 }
