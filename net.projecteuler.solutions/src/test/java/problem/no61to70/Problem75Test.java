e826d8cbd56c0cce00969589fe69bd5ac1769fa9
Miguel <miguel.trigueira@gmail.com>
2024-11-25 12:15:37 +0000

    Problem 75 GREEN

---
+++ net.projecteuler.solutions/src/test/java/problem/no61to70/Problem75Test.java
@@ -0,0 +1,27 @@
+package problem.no61to70;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem75Test {
+    @Test
+    void countSingularRightTriangles12() {
+        assertEquals(1,Problem75.countSingularRightTriangles(12));
+    }
+
+    @Test
+    void countSingularRightTriangles48() {
+        assertEquals(6,Problem75.countSingularRightTriangles(48));
+    }
+
+    @Test
+    void countSingularRightTriangles100() {
+        assertEquals(11,Problem75.countSingularRightTriangles(100));
+    }
+
+    @Test
+    void countSingularRightTriangles1000() {
+        assertEquals(112,Problem75.countSingularRightTriangles(1000));
+    }
+}
