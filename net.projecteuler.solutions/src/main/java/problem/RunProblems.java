4f162a858e18def3d06218341ca35af303d08509
Miguel <miguel.trigueira@gmail.com>
2024-11-26 08:59:17 +0000

    Trace Hiezenbug

--- net.projecteuler.solutions/src/main/java/problem/RunProblems.java
+++ net.projecteuler.solutions/src/main/java/problem/RunProblems.java
@@ -3,30 +3,32 @@ package problem;
 import utils.Stopwatch;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RunProblems {
-    public static final String[] O = {};
-    public static final int[] SLOW = {60};
-    public static final int[] NOT_SLOW = IntStream.range(1, 100)
+    private static final String[] O = {};
+    public static final int[] SLOW = {};
+    public static final int[] FAST = IntStream.range(1, 100)
             .filter(i -> Arrays.stream(SLOW).noneMatch(n -> n == i))
             .toArray();
-    public static final int TOO_SLOW_THRESHOLD_MS = 1000;
-    public static boolean tooSlow = false;
-    private static boolean anyFailed;
+    private static final int TOO_SLOW_THRESHOLD_MS = 1000;
+    private static boolean tooSlow = false;
+    private static final List<Integer> failed = new ArrayList<>();
 
     private RunProblems() {
     }
 
     public static void main(String[] args) {
-        runProblems(SLOW);
+        runProblems(FAST);
     }
 
     public static boolean runProblems(int[] problems) {
         tooSlow = false;
-        anyFailed = false;
+        failed.clear();
         Stopwatch stopwatch = Stopwatch.start();
 
         String results = Arrays.stream(problems)
@@ -38,7 +40,7 @@ public class RunProblems {
         System.out.println(results);
         System.out.println("Total time: " + stopwatch.elapsed() + " ms");
         
-        if(anyFailed) throw new RuntimeException("Problems failed");
+        if(!failed.isEmpty()) throw new RuntimeException("Problems failed. "+failed);
 
         return tooSlow;
     }
@@ -50,24 +52,25 @@ public class RunProblems {
 
     private static String runMain(String s) {
         Stopwatch stopwatch1 = Stopwatch.start();
-        boolean failed = false;
+        boolean failedFlag = false;
         int i = problemNumber(s);
         try {
             Class<?> clazz = Class.forName(s);
             System.out.print(i + " ");
             Method main = clazz.getMethod("main", String[].class);
-            failed = true;
+            failedFlag = true;
             main.invoke(null, (Object) O);
-            failed = false;
+            failedFlag = false;
         } catch (Exception ignored) {
         }
         if(stopwatch1.elapsed() > TOO_SLOW_THRESHOLD_MS) {
             System.err.println("Problem " + i + " took " + stopwatch1.elapsed() + " ms");
             tooSlow = true;
         }
-        if(failed)
-            anyFailed = true;
-        return (i + "," + stopwatch1.elapsed() + "," + (failed ? "FAILED" : ""));
+        if(failedFlag)
+            failed.add(i);
+
+        return (i + "," + stopwatch1.elapsed() + "," + (failedFlag ? "FAILED" : ""));
     }
 
     private static int problemNumber(String s) {
