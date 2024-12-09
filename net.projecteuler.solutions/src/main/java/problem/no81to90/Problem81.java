b01305584003957ee88909d026e4e07e8045259e
Miguel <miguel.trigueira@gmail.com>
2024-12-09 08:58:49 +0000

    Problem 81 BLUE

--- net.projecteuler.solutions/src/main/java/problem/no81to90/Problem81.java
+++ net.projecteuler.solutions/src/main/java/problem/no81to90/Problem81.java
@@ -5,12 +5,13 @@ import utils.data.Grid;
 import utils.data.GridNumber;
 
 import static problem.Solution.problem;
+import static utils.FileUtils.concat;
 
 public class Problem81 {
     public static void main(String[] args) {
         // https://projecteuler.net/problem=81
         problem("Path sum: Two ways", () ->
-                solution(Grid.of(GridNumber.toShortGrid(FileUtils.getString("/0081_matrix.txt").replaceAll(","," ").lines()))));
+                solution(Grid.of(GridNumber.toShortGrid(concat(FileUtils.getString("/0081_matrix.txt"),"1").replaceAll(","," ").lines()))));
     }
 
     static long solution(Grid grid) {
