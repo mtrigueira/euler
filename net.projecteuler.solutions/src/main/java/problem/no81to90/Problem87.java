9b3cec5f8e97efce93f106650340098fc48239d8
Miguel <miguel.trigueira@gmail.com>
2024-12-18 15:20:39 +0000

    Problem 88 GREEN

--- net.projecteuler.solutions/src/main/java/problem/no81to90/Problem87.java
+++ net.projecteuler.solutions/src/main/java/problem/no81to90/Problem87.java
@@ -2,6 +2,9 @@ package problem.no81to90;
 
 import utils.prime.Prime;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static problem.Solution.problem;
 
 public class Problem87 {
@@ -12,26 +15,29 @@ public class Problem87 {
     }
 
     static int countPrimePowerTriplesBelow(int limit) {
-        int count = 0;
         int squares = (int) Math.pow(limit, 1 / 2.0);
         int[][] raised = new int[squares][5];
         int[] primes = Prime.primeFactorBuilder(squares).stream().mapToInt(x -> x).toArray();
+        Set<Integer> uniques = new HashSet<>();
 
         for (int pow = 2; pow <= 4; pow++)
-            for (int i = 0; i < primes.length; i++)
-                raised[i][pow] = (int) Math.pow(primes[i], pow);
+            for (int i = 0; i < primes.length; i++) {
+                double d = Math.pow(primes[i], pow);
+                raised[i][pow] = d > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) d;
+            }
 
-        for (int k = 0; k < primes.length; k++)
+        for (int k = 0; k < primes.length; k++) {
+            if (raised[k][4] > limit) break;
             for (int j = 0; j < primes.length; j++) {
-                int part = raised[k][4] + raised[j][3];
+                long part = raised[k][4] + raised[j][3];
                 if (part >= limit) break;
                 for (int i = 0; i < primes.length; i++) {
                     if (part + raised[i][2] >= limit) break;
-                    count++;
+                    uniques.add((int)(part+raised[i][2]));
                 }
             }
-
-        return count;
+        }
+        return uniques.size();
     }
 
     private Problem87() {
