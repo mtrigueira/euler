6e9e59680ae8ec926a9fd1405dd0e72034cbb9d1
Miguel <miguel.trigueira@gmail.com>
2024-12-28 11:15:37 +0000

    Problem 91 GREEN

---
+++ net.projecteuler.solutions/src/test/java/problem/no91to100/Problem91Test.java
@@ -0,0 +1,12 @@
+package problem.no91to100;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem91Test {
+    @Test
+    void solution() {
+        assertEquals(14, Problem91.solution(2));
+    }
+}
