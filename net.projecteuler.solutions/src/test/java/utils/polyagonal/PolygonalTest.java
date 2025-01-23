1a64865f2b3e38ea02d2d1b47733f77dd85296fd
Miguel <miguel.trigueira@gmail.com>
2025-01-23 14:48:06 +0000

    Hide constructor. Take int for sides instead of bigInteger. Pre-compute constant.

--- net.projecteuler.solutions/src/test/java/utils/polyagonal/PolygonalTest.java
+++ net.projecteuler.solutions/src/test/java/utils/polyagonal/PolygonalTest.java
@@ -1,19 +1,5 @@
 package utils.polyagonal;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static utils.BigIntegerConstants.big;
-
 public class PolygonalTest {
-    @Test
-    void test() {
-        assertEquals("Polygonal{sides=100}", new Polygonal(big(100), "").toString());
-    }
-
-    @Test
-    void testNull() {
-        assertThrows(IllegalArgumentException.class, ()-> new Polygonal(null, "").toString());
-    }
+// See individual sorts of polygonals that have dedicated tests
 }
