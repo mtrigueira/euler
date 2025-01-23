905be7f4a27f6572a4ac3c9d30a8c2929343e326
Miguel <miguel.trigueira@gmail.com>
2025-01-23 15:17:12 +0000

    Internal representation to long from BigInteger

--- net.projecteuler.solutions/src/main/java/utils/polyagonal/Polygonal.java
+++ net.projecteuler.solutions/src/main/java/utils/polyagonal/Polygonal.java
@@ -3,8 +3,6 @@ package utils.polyagonal;
 import java.math.BigInteger;
 import java.util.List;
 
-import static utils.BigIntegerConstants.*;
-
 public class Polygonal {
     public static final Polygonal TRIANGULAR = new Polygonal(3, "Triangular");
     public static final Polygonal SQUARE = new Polygonal(4, "Square");
@@ -14,24 +12,25 @@ public class Polygonal {
     public static final Polygonal OCTAGONAL = new Polygonal(8, "Octagonal");
     public static final List<Polygonal> POLYGONALS = List.of(TRIANGULAR, SQUARE, PENTAGONAL, HEXAGONAL, HEPTAGONAL, OCTAGONAL);
 
-    private final BigInteger sMinus2;
-    private final BigInteger sMinus4;
-    private final BigInteger sMinus4Squared;
+    private final long sMinus2;
+    private final long sMinus4;
+    private final long sMinus4Squared;
+    private final long twoMultiplySMinus2;
+    private final long sMinus2multiply8;
     private final String name;
-    private final BigInteger twoMultiplySMinus2;
-    private final BigInteger sMinus2multiply8;
 
     public boolean is(BigInteger x) {
         return at(indexOf(x)).equals(x);
     }
 
     private BigInteger indexOf(BigInteger x) {
-        return ((x.multiply(sMinus2multiply8).add(sMinus4Squared))
-                .sqrt().add(sMinus4)).divide(twoMultiplySMinus2);
+        return BigInteger.valueOf(((long) Math.sqrt((x.longValueExact() * sMinus2multiply8 + sMinus4Squared)
+        ) + sMinus4) / twoMultiplySMinus2);
     }
 
     public BigInteger at(BigInteger n) {
-        return sMinus2.multiply(n.multiply(n)).subtract(sMinus4.multiply(n)).divide(TWO);
+        long en = n.longValueExact();
+        return BigInteger.valueOf((sMinus2 * en * en - sMinus4 * en) / 2);
     }
 
     @Override
@@ -41,10 +40,10 @@ public class Polygonal {
 
     private Polygonal(int sides, String name) {
         this.name = name;
-        this.sMinus2 = BigInteger.valueOf(sides-2);
-        this.sMinus4 = BigInteger.valueOf(sides-4);
-        sMinus4Squared = sMinus4.multiply(sMinus4);
-        twoMultiplySMinus2 = TWO.multiply(sMinus2);
-        sMinus2multiply8 = sMinus2.multiply(EIGHT);
+        this.sMinus2 = (sides - 2);
+        this.sMinus4 = (sides - 4);
+        sMinus4Squared = sMinus4 * (sMinus4);
+        twoMultiplySMinus2 = 2 * (sMinus2);
+        sMinus2multiply8 = sMinus2 * (8);
     }
 }
--- net.projecteuler.solutions/src/test/java/utils/polyagonal/PolygonalTest.java
+++
@@ -1,5 +0,0 @@
-package utils.polyagonal;
-
-public class PolygonalTest {
-// See individual sorts of polygonals that have dedicated tests
-}
